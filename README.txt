450df3c94ddcb576767c217df766e2dd6ce39c0e
Austin Ginder <austinginder@gmail.com>
2021-01-11 18:09:56 -0500

    🚀 RELEASE: Version 1.0

---
+++ README.txt
@@ -0,0 +1,16 @@
+=== CaptainCore Supporting Files ===
+Author URI: https://austinginder.com
+Plugin URI: https://captaincore.io
+Contributors: austinginder
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: MIT License
+License URI: https://opensource.org/licenses/MIT
+
+Supporting files for CaptainCore toolkit.
+
+== Changelog ==
+
+= 1.0.0: January 11, 2021 =
+* Add package [HTML2PDF](https://github.com/spipu/html2pdf)
---
+++ captaincore-supporting-files.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       CaptainCore Supporting Files
+ * Plugin URI:        https://captaincore.io
+ * Description:       Supporting files for CaptainCore toolkit.
+ * Version:           1.0.0
+ * Author:            Austin Ginder
+ * Author URI:        https://austinginder.com
+ * License:           MIT License
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       captaincore-supporting-files
+ * Domain Path:       /languages
+ */
+
+require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
